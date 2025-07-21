aa577c418457fff0d23e318ae2da32cb23df7a79
egile01 <akashjagadale001@gmail.com>
2025-07-21 19:33:19 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+function showMessage() {
+  document.getElementById("message").innerText = 
+    "Sonali madam, having you in my life means a lot. The way you talk, your smile, and just your presence – it all gives me peace. If I’ve ever messed up, I’m really sorry. You’re honestly very special to me.";
+}
